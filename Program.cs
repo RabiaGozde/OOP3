b11f87df158332b7788c95dbb89dd9e554398130
RAßİA GÖZD€ <gozde.rabia@hotmail.com>
2021-02-03 20:30:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IhtiyacKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            TasıtKrediManager tasıtKrediManager = new TasıtKrediManager();
+            KonutKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(konutKrediManager, databaseLoggerService);
+
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, tasıtKrediManager};
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+            
+
+        }
+    }
+}
